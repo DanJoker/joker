226e7edb5835d3373cc7d118becf8ca9844fad41
Joker <1047732652@qq.com>
2018-01-30 10:34:10 +0800

    joker:new joker-util project

---
+++ joker-util/src/main/java/cn/joker/util/FileUtil.java
@@ -0,0 +1,5 @@
+package cn.joker.util;
+
+public class FileUtil {
+
+}
---
+++ joker-util/src/test/java/cn/joker/util/RedisUtilTest.java
@@ -0,0 +1,12 @@
+package cn.joker.util;
+
+import junit.framework.TestCase;
+
+public class RedisUtilTest extends TestCase {
+    
+	public void test() {
+		
+		RedisUtil.set("name", "joker");
+		
+	}
+}
